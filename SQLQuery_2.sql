2a2887e1b44a506615ea9187ba95fb996af0bba9
tanhiroshi <hirtan@microsoft.com>
2022-02-03 17:12:26 +0900

    test22

---
+++ SQLQuery_2.sql
@@ -0,0 +1,10 @@
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Employee4](
+	[No] [int] NOT NULL,
+	[Name] [nchar](15) NOT NULL,
+	[RegDate] [date] NOT NULL
+) ON [PRIMARY]
+GO
