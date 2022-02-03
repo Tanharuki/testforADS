dc0db55812454582cd3a8474920f2938430dc567
tanhiroshi <hirtan@microsoft.com>
2022-02-03 12:53:15 +0900

    test

---
+++ SQLQuery_1.sql
@@ -0,0 +1,10 @@
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Employee](
+	[No] [int] NOT NULL,
+	[Name] [nchar](15) NOT NULL,
+	[RegDate] [date] NOT NULL
+) ON [PRIMARY]
+GO
